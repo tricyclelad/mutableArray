8da30e33611e16bd0c81e64758bd30a7558c2257
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2017-06-06 16:37:10 -0600

    Added testing code for main to view progress on mutable array

--- mutableArray/main.cpp
+++ mutableArray/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include "mutableArray.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
+
+	mutableArray A;
+
+	for (int i = 0; i < A.capacity(); i++)
+		A.push_back(i);
+	
+	for (int i = 0; i < A.size(); i++)
+		cout << A[i] << endl;
+
+
 	return 0;
 }
