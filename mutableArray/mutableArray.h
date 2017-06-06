826d37ecd69ea4179ad77c131ef3c41379186948
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2017-06-06 16:40:15 -0600

    Changed #pragma once to a more traditional #ifndef scheme for this header.
    Changed the constructor to a default constructor of size 10.
    
    Added inline functions for size and capacity.
    
    Added more private variables: vSize, vCapacity, and *vArray.

--- mutableArray/mutableArray.h
+++ mutableArray/mutableArray.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef MUTABLEARRAY_H
+#define MUTABLEARRAY_H
 
 /*Requirements for the mutable array or Vector to be valid.
 -------------------------------------------------------------
@@ -18,23 +19,28 @@ front, back			O(1)
 class mutableArray
 {
 public:
-	mutableArray();
+	mutableArray(int n = 10);
 	~mutableArray();
-	int size(); //returns the current number of elements of the mutable array
-	int capacity(); //returns the maximum number of elements of the mutable array
+	int size() { return vSize; }; //returns the current number of elements of the mutable array
+	int capacity() { return vCapacity; }; //returns the maximum number of elements of the mutable array
 	bool is_empty(); //returns true if array is empty, false if not.
-	void push_back(); //adds an element to the end of the vector.
+	void push_back(int item); //adds an element to the end of the vector.
 	void insert(); //insert an element at the index, shifts that index's value and trailing elements.
-	void at(int index); //returns item at given index, error checks for index out of bounds
+	//void at(int index); //returns item at given index, error checks for index out of bounds **NO LONGER NEEDED**
 	void prepend(int element); //insert item at 0 and shift trailing indexes
 	void pop(); //remove from end, return value
 	void deleteAt(int index); //delete item at index, shifting all trailing elements left
 	void remove(int item); //looks for a value and removes that index holding. Allows multiple removals.
 	int find(int item); //looks for a value and returns the first index with that value, returns -1 if not found
+	
+	int &operator [] (unsigned int i); //overloads the [] operator to allow array-like indexing and such
 private:
-	void resize(int new_capacity); //When capacity is reached, double the capacity of the array.
+	void resize(int new_capacity, bool copy); //When capacity is reached, double the capacity of the array.
 								   //When popping an item, if size is 1/4 capacity, halve the capacity of the array.
-
+	int vSize;
+	int vCapacity;
+	int *vArray = nullptr;
 
 };
 
+#endif //MUTABLEARRAY_H
